f03f0f36484fdc11baf3fbeccbc0274cd43040d3
kimdongyoon488 <rlaehddbs54321@gmail.com>
2025-06-30 21:29:28 +0900

    feat : 디렉터리 구조 및 추후 챗봇 개발을 위한 모델 추론 라우터 초기 세팅

--- server/main.py
+++ server/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from server.routers import test
+from server.routers import inference
 
 app = FastAPI()
 
 # 테스트용 라우터
-app.include_router(test.router)
+# app.include_router(test.router)
+
+# 추후 챗봇 개발을 위한 기본적인 모델 추론 라우터 구성
+app.include_router(inference.router)
+
