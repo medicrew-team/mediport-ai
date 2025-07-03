ca8d6d2c8d3fb38927d746aa1e79d4fc86affb4e
kimdongyoon488 <rlaehddbs54321@gmail.com>
2025-07-03 22:32:37 +0900

    refactor: 실행 환경별 환경 변수 분리를 위해 .env 구조 개선 및 리팩토링 (#4)
    - 기존에는 하나의 .env 파일에서 개발/배포 환경을 모두 관리 하여 실행 환경을 변경 할때마다 일일이 모두 수정해줘야 하는 구조였지만 .env.(local/cloud)로 분리하여 .env 파일에서 실행 환경 코드만 수정하면 실행 환경에 맞는 .env 파일이 실행되도록 구조를 개선

--- server/config.py
+++ server/config.py
@@ -1,10 +1,24 @@
-### server\.env 에 있는 환경 변수 들을 불러 와주는 중간 다리 역할을 하는 파일 ###
+### 실행 환경에 맞는 환경 변수 들을 불러 와주는 중간 다리 역할을 하는 파일 ###
 
 import os
 from dotenv import load_dotenv
 
-# server\.env 파일 내용을 불러옴
-load_dotenv()
+# 기본 .env 파일에서 실행 환경(local or cloud) 정보만 불러옴
+load_dotenv(dotenv_path="envs/.env")
+env_mode = os.getenv("ENV_MODE")
 
-# 환경 변수 'LOCAL_COLAB_SERVER_URL' 값을 가져옴
-LOCAL_COLAB_SERVER_URL = os.getenv("LOCAL_COLAB_SERVER_URL")
+# 실행 환경에 맞는 .env 파일 경로 생성
+env_file_path = f"envs/.env.{env_mode}"
+
+# 해당 .env 파일이 존재하면 환경 변수로 로딩
+if os.path.exists(env_file_path):
+    load_dotenv(dotenv_path=env_file_path, override=True)
+else:
+    raise FileNotFoundError(f"{env_file_path} not found.")
+
+# 개발 환경일 때만 사용하는 변수
+if env_mode == "local":
+    LOCAL_COLAB_SERVER_URL = os.getenv("LOCAL_COLAB_SERVER_URL")
+
+
+# 공통 환경 변수
