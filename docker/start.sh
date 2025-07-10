058982dac26d8182352b6bba5d38d7acb54707ee
kimdongyoon488 <rlaehddbs54321@gmail.com>
2025-07-10 17:10:03 +0900

    feat : Dockerfile 및 배포 스크립트 작성, 도커 이미지 빌드 (#6)

---
+++ docker/start.sh
@@ -0,0 +1,11 @@
+echo "FAISS 벡터 DB 저장 여부 확인 중..."
+
+if [ ! -f "./data/medicine_faiss.index" ]; then
+    echo "벡터 DB가 없어서 save_to_faiss.py 실행합니다."
+    python server/save_to_faiss.py
+else
+    echo "벡터 DB가 이미 존재합니다. 그대로 서버 실행합니다."
+fi
+
+echo "FastAPI 서버 실행 시작"
+uvicorn server.main:app --host 0.0.0.0 --port 8000
