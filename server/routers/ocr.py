237bf74b36e8625e12b2a4a2584f0465063014c0
kimdongyoon488 <rlaehddbs54321@gmail.com>
2025-07-28 22:37:39 +0900

    feat : 약 봉투 스캔시 제품명(약명)의 X축을 기준으로 제품명의 복약 정보를 추출하는 기능 개발중 (#4)

--- server/routers/ocr.py
+++ server/routers/ocr.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, File, UploadFile, HTTPException
-from server.services.ocr.vision_api import extract_text_from_image, match_table_format
+from server.services.ocr.vision_api import extract_text_from_image
 from server.services.ocr.preprocess import preprocess_text
+from server.services.ocr.extract_medicine_names import extract_medicine_names_from_text
+from server.services.ocr.medicine_dosage_map import map_medicines_to_dosages
+
 
 router = APIRouter()
 
@@ -14,15 +17,28 @@ async def ocr_image(file: UploadFile = File(...)):
         content = await file.read()
 
         # 텍스트 추출 함수 실행
-        raw_text = extract_text_from_image(content)
+        ocr_result  = extract_text_from_image(content)
+
+        raw_text = ocr_result["text"]
+        words = ocr_result["words"]
+
+        # 약명 리스트
+        medicine_names = extract_medicine_names_from_text(raw_text)
+
+        # 복약 정보 매핑
+        mapped_result = map_medicines_to_dosages(raw_text, words, medicine_names)
 
         # 추출한 텍스트를 전처리 하여 깔끔한 형태로 정리
         # cleaned_text = preprocess_text(raw_text)
 
         # 표 기반 매핑 실행
-        result = match_table_format(raw_text)
+        # result = match_table_format(raw_text)
 
-        return {"parsed_result": result}
+        return {
+            "raw_text": raw_text,
+            "medicine_names": medicine_names,
+            "mapped_result" : mapped_result
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
