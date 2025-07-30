2a24f4740b332bd485f103c3cb9ccd913297bb79
Won_Woo_2 <lww000510@naver.com>
2025-07-30 15:21:20 +0900

    feat : 데이터셋 구조 변경 사항에 맞게 프롬프트 수정 (#5-2)

--- server/services/prompt/builder.py
+++ server/services/prompt/builder.py
@@ -1,4 +1,3 @@
-
 def get_purchase_info(doc: dict) -> str:
     """
     '구분' 필드에 따라 구매 장소 문장 리턴
@@ -21,7 +20,6 @@ def format_field(label: str, value: str) -> str:
 
 
 def build_prompt(user_input: str, doc: dict) -> str:
-
     제품명 = doc.get("제품명", "").strip()
     성분명 = doc.get("성분명", "").strip()
 
@@ -29,15 +27,16 @@ def build_prompt(user_input: str, doc: dict) -> str:
 
     fields = [
         ("약품 구분", "일반의약품"),
+        # ("약품 형태 및 제형", doc.get("제형")),
+        # ("약품 용량", doc.get("용량")),
+        # ("기능적 약효 분류", doc.get("BIT")),
+        # ("의약품 적용부위", doc.get("대분류")),
         # ("제품명", doc.get("제품명")),
-        ("약품 제형", doc.get("제형")),
-        ("약품 용량", doc.get("용량","")),
-        # ("약품 작용부위", doc.get("대분류"))
         ("사용되는 증상", doc.get("ICD")),
         ("성분명", doc.get("성분명")),
         ("복용법 또는 복용 주의사항", doc.get("복용법")),
-        ("복용 또는 사용 금기 대상", doc.get("주의사항_금기대상", "")),
-        ("약품 보관법", doc.get("약품 보관법", "") ),
+        ("복용 또는 사용 금기 대상", doc.get("금기대상", "")),
+        ("약품 보관법", doc.get("약품 보관법", "")),
         ("일상 생활에서의 주의사항", doc.get("일상 상호작용", "")),
         (f"{doc.get('제품명')}와(과) 같이 사용하거나 복용하면 안되는 의약품", doc.get("의약품 상호작용", "")),
         (f"{doc.get('제품명')}과(와) {doc.get('의약품 상호작용', '')}을(를) 같이 사용하거나 복용할 경우 나타날 수 있는 이상반응", doc.get("이상 반응", "")),
@@ -49,7 +48,7 @@ def build_prompt(user_input: str, doc: dict) -> str:
     formatted_sections = [format_field(label, value) for label, value in fields if value.strip()]
     section_text = "\n\n".join(formatted_sections)
 
-    prompt =  f"""
+    prompt = f"""
 너는 사용자의 복약 관련 질문에 대해 정중하고 간결하게 응답하는 약 정보 안내 챗봇이야.  
 아래의 지시사항을 무조건 따르도록 해. 
 
@@ -63,15 +62,18 @@ def build_prompt(user_input: str, doc: dict) -> str:
 - '{성분명}'(들)은 절대 한글로 번역하지 말고 답변 문장 내에서 반드시 영문 그대로 해라.
 - 아래에 제공된 약 이름과 약 정보 외의 그 어떤 지식도 참고하지 마라.
 
+
+
+
 [사용자 질문]
 {user_input}
-         
+
 [약 이름]
 {제품명} 
 
 [약 정보]
 {section_text}
- 
+
 Answer:"""
 
     return prompt
